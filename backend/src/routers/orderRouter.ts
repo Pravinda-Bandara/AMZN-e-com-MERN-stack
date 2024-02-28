23d4288f55a73f1cfe6b128c4e0b31423183b703
Pravinda-Bandara <bandarapravinda@gmail.com>
2024-02-28 14:37:04 +0530

    implement /mine api

--- backend/src/routers/orderRouter.ts
+++ backend/src/routers/orderRouter.ts
@@ -1,9 +1,36 @@
 import express,{Request,Response} from "express";
-import {isAuth} from "../utils.js";
+import {isAdmin, isAuth} from "../utils.js";
 import asyncHandler from "express-async-handler";
 import {OrderModel} from "../models/orderModel.js";
 import {Product} from "../models/productModel.js";
 export const orderRouter=express.Router();
+orderRouter.get(
+    '/mine',
+    isAuth,
+    asyncHandler(async (req: Request, res: Response) => {
+
+        const orders = await OrderModel.find({ user: req.body.user._id })
+        res.send(orders)
+
+    })
+)
+
+orderRouter.delete(
+    '/:id',
+    isAuth,
+    asyncHandler(async (req: Request, res: Response) => {
+        const orderId = req.params.id;
+
+            // Find the order by ID and delete it
+            const deletedOrder = await OrderModel.findByIdAndDelete(orderId);
+
+            if (deletedOrder) {
+                res.json({ message: 'Order deleted successfully', order: deletedOrder });
+            } else {
+                res.status(404).json({ message: 'Order not found' });
+            }
+    })
+)
 
 orderRouter.get(
     '/:id',
@@ -64,3 +91,56 @@ orderRouter.put(
         }
     })
 )
+
+
+
+/*
+orderRouter.get(
+    '/',
+    isAuth,
+    isAdmin,
+    asyncHandler(async (req: Request, res: Response) => {
+        const orders = await OrderModel.find().populate('user', 'name')
+        res.send(orders)
+    })
+)
+
+orderRouter.put(
+    '/:id/deliver',
+    isAuth,
+    isAdmin,
+    asyncHandler(async (req: Request, res: Response) => {
+        const order = await OrderModel.findById(req.params.id)
+        if (order) {
+            order.isDelivered = true
+            order.deliveredAt = new Date(Date.now())
+            // order.deliveredAt = Date.now();
+
+            const updatedOrder = await order.save()
+            res.send({ message: 'Order Delivered', order: updatedOrder })
+        } else {
+            res.status(404).send({ message: 'Order Not Found' })
+        }
+    })
+)
+
+orderRouter.delete(
+    '/:id',
+    isAuth,
+    isAdmin,
+    asyncHandler(async (req: Request, res: Response) => {
+        const order = await OrderModel.findById(req.params.id)
+        if (order) {
+            const deleteOrder = await order.deleteOne()
+            res.send({ message: 'Order Deleted', order: deleteOrder })
+        } else {
+            res.status(404).send({ message: 'Order Not Found' })
+        }
+    })
+)
+
+
+
+*/
+
+
