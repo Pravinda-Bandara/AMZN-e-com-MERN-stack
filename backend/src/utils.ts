5fad854c0a2bfd665b4ac11bcb8a08c76e3fcb98
Pravinda-Bandara <bandarapravinda@gmail.com>
2024-02-28 14:36:22 +0530

    implement isAdmin middleware

--- backend/src/utils.ts
+++ backend/src/utils.ts
@@ -37,3 +37,34 @@ export const isAuth=(req:Request,res:Response,next:NextFunction)=>{
         res.status(401).json({ message: 'NO Token' });
     }
 }
+
+export const isAdmin = (req: Request, res: Response, next: NextFunction) => {
+    // @ts-ignore
+    if (req.user && req.user.isAdmin) {
+        next()
+    } else {
+        res.status(401).send({ message: 'Invalid Admin Token' })
+    }
+}
+
+/*export const isAuth = (req: Request, res: Response, next: NextFunction) => {
+    const { authorization } = req.headers;
+    if (authorization) {
+        const token = authorization.slice(7, authorization.length); // Bearer xxx
+        const decode = jwt.verify(
+            token,
+            process.env.JWT_SECRET || 'somethingsecret'
+        );
+        req.body.user = decode as {
+            _id: string;
+            name: string;
+            email: string;
+            isAdmin: boolean;
+            token: string;
+        };
+        next();
+    } else {
+        res.status(401).json({ message: 'No Token' });
+    }
+};*/
+
