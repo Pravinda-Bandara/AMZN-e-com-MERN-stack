f703740e3cac6bfe08a0e4c5117595ee6cdf6361
Pravinda-Bandara <bandarapravinda@gmail.com>
2024-03-02 19:31:10 +0530

    implement admin product get,delete,create routes

--- backend/src/routers/productRouter.ts
+++ backend/src/routers/productRouter.ts
@@ -1,8 +1,9 @@
-import express from "express";
+import express,{Request,Response} from "express";
 import asyncHandler from 'express-async-handler'
-import {ProductModel} from "../models/productModel.js";
-import {isAuth} from "../utils.js";
+import {Product, ProductModel} from "../models/productModel.js";
+import {isAdmin, isAuth} from "../utils.js";
 
+const PAGE_SIZE = 3
 export const productRouter = express.Router()
 // /api/products
 productRouter.get(
@@ -12,6 +13,7 @@ productRouter.get(
         res.json(products)
     })
 )
+
 
 // /api/slug/tshirt
 productRouter.get(
@@ -26,4 +28,67 @@ productRouter.get(
     })
 )
 
+
+productRouter.get(
+    '/admin',
+    isAuth,
+    isAdmin,
+    asyncHandler(async (req: Request, res: Response) => {
+        const { query } = req
+        const page = Number(query.page || 1)
+        const pageSize = Number(query.pageSize) || PAGE_SIZE
+
+        const products = await ProductModel.find()
+            .skip(pageSize * (page - 1))
+            .limit(pageSize)
+        const countProducts = await ProductModel.countDocuments()
+        res.send({
+            products,
+            countProducts,
+            page,
+            pages: Math.ceil(countProducts / PAGE_SIZE),
+        })
+    })
+)
+
+productRouter.post(
+    '/',
+    isAuth,
+    isAdmin,
+    asyncHandler(async (req: Request, res: Response) => {
+        const product = await ProductModel.create({
+            name: 'sample name ' + Date.now(),
+            image: '../assets/images/p1.jpg',
+            price: 0,
+            slug: 'sample-slug-' + Date.now(),
+            category: 'sample category',
+            brand: 'sample brand',
+            countInStock: 0,
+            rating: 0,
+            numReviews: 0,
+            description: 'sample description',
+        } as Product)
+
+        const createdProduct = await product.save()
+        res.send({
+            message: 'Product Created',
+            product: createdProduct,
+        })
+    })
+)
+
+productRouter.delete(
+    '/:id',
+    isAuth,
+    isAdmin,
+    asyncHandler(async (req: Request, res: Response) => {
+        const product = await ProductModel.findById(req.params.id)
+        if (product) {
+            const deleteProduct = await product.deleteOne()
+            res.send({ message: 'Product Deleted', product: deleteProduct })
+        } else {
+            res.status(404).send({ message: 'Product Not Found' })
+        }
+    })
+)
 
