fb54944a90eed9dac85e9f3895841a2626bfec5c
Pravinda-Bandara <bandarapravinda@gmail.com>
2024-11-29 01:17:38 +0530

    add admin to Navbar

--- frontend/src/pages/AdminPages/AdminDashboard/AdminDashboard.tsx
+++ frontend/src/pages/AdminPages/AdminDashboard/AdminDashboard.tsx
@@ -10,6 +10,9 @@ export default function AdminDashboard() {
         <Col md={3} className="bg-gray-800 min-h-screen text-white">
           <h2 className="text-center py-4">Admin Panel</h2>
           <Nav className="flex-column px-3">
+          <Nav.Link as={Link} to="/admin" className="text-gray-300 hover:text-white py-2">
+              Dashboard
+            </Nav.Link>
             <Nav.Link as={Link} to="/admin/orders" className="text-gray-300 hover:text-white py-2">
               Orders
             </Nav.Link>
